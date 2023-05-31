6d915ef9ff304ba8235c10cd18b7452ffb01a650
Jantopthegreat <fauzangriya@gmail.com>
2023-05-31 11:14:40 +0700

    class budi dengan pekerja dan pelajar

--- Problem Multi Waris/Problem Multi Waris.cpp
+++ Problem Multi Waris/Problem Multi Waris.cpp
@@ -33,3 +33,14 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
